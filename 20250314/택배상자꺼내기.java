e4b6dd62c649985cca299786126684ec916f207d
subo-9439 <kws3363@gmail.com>
2025-03-11 22:49:34 +0900

    둘째주 택배상자꺼내기, 숫자짝꿍 풀이

---
+++ 20250314/택배상자꺼내기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+        int rows = (n + w - 1) / w;
+        int[][] map = new int[rows][w];
+        int val = 0, xIdx = 0, yIdx = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < w; j++) {
+                val++;
+
+                int col = (i % 2 == 0) ? j : (w - j - 1);
+                map[i][col] = val;
+
+                if (val == num) {
+                    xIdx = i;
+                    yIdx = col;
+                }
+                if (val == n)
+                    break;
+            }
+            if (val == n)
+                break;
+        }
+        return (map[rows - 1][yIdx] == 0) ? rows - xIdx - 1 : rows - xIdx;
+    }
+}
